dc347da7760a067273a1b739ce1a6f439117a491
Sakib Rahman <seu.sakib@gmail.com>
2026-02-18 16:38:25 +0600

    setup better auth

---
+++ src/app/lib/auth.ts
@@ -0,0 +1,41 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { Role, Status } from "../../generated/prisma/enums";
+import { prisma } from "./prisma";
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+    emailAndPassword: { enabled: true },
+
+    user: {
+        additionalFields: {
+            role: {
+                type: "string",
+                required: true,
+                defaultValue: Role.PATIENT,
+            },
+            status: {
+                type: "string",
+                required: true,
+                defaultValue: Status.ACTIVE,
+            },
+            needPasswordChange: {
+                type: "boolean",
+                required: true,
+                defaultValue: false,
+            },
+            isDeleted: {
+                type: "boolean",
+                required: true,
+                defaultValue: false,
+            },
+            deletedAt: {
+                type: "date",
+                required: false,
+                defaultValue: null,
+            },
+        },
+    },
+});
